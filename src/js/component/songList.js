b5c4a9c0971725d8ae05a11ba4862a7717de5c6b
JuanLGC <juangcardinale@gmail.com>
2020-10-27 10:11:31 +0000

    Spotify Player Corrected

--- src/js/component/songList.js
+++ src/js/component/songList.js
@@ -6,6 +6,10 @@ export default function MusicPlayer() {
 	let [songUrl, setSongUrl] = useState(
 		"https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3"
 	);
+	let [halfLink, setHalfLink] = useState(
+		"https://assets.breatheco.de/apis/sound/"
+	);
+	let [isPlaying, setIsPlaying] = useState(false);
 	const musicPlayer = document.querySelector("#musicPlayer");
 	//  Creating Array of songs
 
@@ -19,48 +23,158 @@ export default function MusicPlayer() {
 			});
 	}, []);
 
-	console.log(songUrl);
-	return (
+	const previousSong = () => {
+		for (let i = 0; i < arraysongs.length; i++) {
+			if (halfLink + arraysongs[i].url == songUrl) {
+				setSongUrl(
+					"https://assets.breatheco.de/apis/sound/" +
+						arraysongs[i - 1].url
+				);
+				musicPlayer.load();
+				musicPlayer.play();
+				setIsPlaying(true);
+			}
+		}
+	};
+
+	const nextSong = () => {
+		for (let i = 0; i < arraysongs.length; i++) {
+			if (halfLink + arraysongs[i].url == songUrl) {
+				setSongUrl(
+					"https://assets.breatheco.de/apis/sound/" +
+						arraysongs[i + 1].url
+				);
+				musicPlayer.load();
+				musicPlayer.play();
+				setIsPlaying(true);
+			}
+		}
+	};
+
+	const playMusic = () => {
+		if (isPlaying) {
+			musicPlayer.pause();
+			setIsPlaying(false);
+		} else {
+			musicPlayer.load();
+			musicPlayer.play();
+			setIsPlaying(true);
+		}
+	};
+
+	if (arraysongs) {
+		return (
+			<div>
+				<div className="row mt-3">
+					<ol className="col-12 ">
+						{arraysongs
+							? arraysongs.map(songDetails => {
+									return (
+										<li
+											key={songDetails.url}
+											onClick={() => {
+												setSongUrl(
+													"https://assets.breatheco.de/apis/sound/" +
+														songDetails.url
+												);
+												musicPlayer.load();
+												setIsPlaying(true);
+												musicPlayer.play();
+											}}>
+											<span className="ml-3">
+												{songDetails.name}
+											</span>
+										</li>
+									);
+							  })
+							: "Loading..."}
+					</ol>
+				</div>
+				<div className="navbar justify-content-center">
+					<div className="audio-buttons mx-4 my-2">
+						<i
+							className="fa fa-backward fa-2x"
+							aria-hidden="true"
+							onClick={() => {
+								previousSong();
+							}}
+						/>
+					</div>
+					<div className="audio-buttons mx-4 my-2 fa-2x">
+						{isPlaying ? (
+							<i
+								className="fa fa-pause"
+								aria-hidden="true"
+								onClick={() => {
+									playMusic();
+								}}
+							/>
+						) : (
+							<i
+								className="fa fa-play"
+								aria-hidden="true"
+								onClick={() => {
+									playMusic();
+								}}
+							/>
+						)}
+					</div>
+					<div className="audio-buttons mx-4 my-2 fa-2x">
+						<i
+							className="fa fa-forward"
+							aria-hidden="true"
+							onClick={() => {
+								nextSong();
+							}}
+						/>
+					</div>
+				</div>
+				<audio id="musicPlayer">
+					<source src={songUrl} type="audio/mpeg" />
+				</audio>
+			</div>
+		);
+	} else {
 		<div>
 			<div className="row mt-3">
 				<ol className="col-12 ">
-					{arraysongs
-						? arraysongs.map(songDetails => {
-								console.log(songDetails);
-								return (
-									<li
-										key={songDetails.url}
-										onClick={() => {
-											setSongUrl(
-												"https://assets.breatheco.de/apis/sound/" +
-													songDetails.url
-											);
-											musicPlayer.load();
-											musicPlayer.play();
-										}}>
-										<span className="ml-3">
-											{songDetails.name}
-										</span>
-									</li>
-								);
-						  })
-						: "Loading... Wait you son of a bitch"}
+					<li>Loading...Please Wait.</li>
 				</ol>
 			</div>
 			<div className="navbar justify-content-center">
 				<div className="audio-buttons mx-4 my-2">
-					<i className="fa fa-backward fa-2x" aria-hidden="true" />
+					<i
+						className="fa fa-backward fa-2x"
+						aria-hidden="true"
+						onClick={() => {
+							previousSong();
+						}}
+					/>
 				</div>
 				<div className="audio-buttons mx-4 my-2 fa-2x">
-					<i className="fa fa-play" aria-hidden="true" />
+					<i
+						className="fa fa-play"
+						aria-hidden="true"
+						onClick={() => {
+							musicPlayer.load();
+							musicPlayer.play();
+							setIsPlaying(true);
+						}}
+					/>
 				</div>
 				<div className="audio-buttons mx-4 my-2 fa-2x">
-					<i className="fa fa-forward" aria-hidden="true" />
+					<i
+						className="fa fa-forward"
+						aria-hidden="true"
+						onClick={() => {
+							nextSong();
+						}}
+					/>
 				</div>
 			</div>
 			<audio id="musicPlayer">
 				<source src={songUrl} type="audio/mpeg" />
 			</audio>
-		</div>
-	);
+		</div>;
+	}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,9 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 import MusicPlayer from "./component/songList.js";
-//include bootstrap npm library into the bundle
 import "bootstrap";
 
-//include your index.scss file into the bundle
+
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
-
-//render your react application
 ReactDOM.render(<MusicPlayer />, document.querySelector("#app"));
